6d8c8819a8c5781010432c6f1772fb69e21dd4aa
DIKSHATRIVEDI <diksha.trivedi2122@gmail.com>
2025-03-06 02:26:47 +0530

    MySQL setup and crud operation on employee

---
+++ src/main/java/com/example/employeepayrollapp/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.example.employeepayrollapp.controller;
+
+import com.example.employeepayrollapp.model.Employee;
+import com.example.employeepayrollapp.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping("/all")//http://localhost:8080/employeepayrollservice/all
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/get/{id}")//http://localhost:8080/employeepayrollservice/get/1
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = service.getEmployeeById(id);
+        return employee != null ? ResponseEntity.ok(employee) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/create")//http://localhost:8080/employeepayrollservice/create
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
+        return service.createEmployee(employee);
+    }
+
+    @PutMapping("/update/{id}")//http://localhost:8080/employeepayrollservice/update/1
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @Valid @RequestBody Employee employee) {
+        Employee updatedEmployee = service.updateEmployee(id, employee);
+        return updatedEmployee != null ? ResponseEntity.ok(updatedEmployee) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")//http://localhost:8080/employeepayrollservice/delete/1
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
