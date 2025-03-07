3baee48621e91cb9188bcdde085a498dd9eacab3
DIKSHATRIVEDI <diksha.trivedi2122@gmail.com>
2025-03-07 14:37:54 +0530

    Logging levels added

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,14 +3,18 @@ package com.example.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		String activeProfile = context.getEnvironment().getActiveProfiles().length > 0
+				? context.getEnvironment().getActiveProfiles()[0]
+				: "default";
+		log.info("Employee Payroll App Started in {} environment", activeProfile);
 	}
-
 }
+
